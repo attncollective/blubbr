f96daa2d93ccb75daa0328ed1ee5d53cae4c1faf
0xChampi <86300766+0xChampi@users.noreply.github.com>
2022-09-22 16:46:45 -0400

    styling

--- frontend/components/TrendingNFT.js
+++ frontend/components/TrendingNFT.js
@@ -39,15 +39,14 @@ export default function TrendingNFT({ name, address, symbol, totalSales, floor, 
     // }, [])
 
     return (
-        <table>
-            <tr className="w-3/4 gray-500 border-b border-gray-400 w-40">
-            <td scope="row" className="py-4 px-6 font-medium text-gray-50 dark:text-gray-100 w-40">{name}</td>
-            <td scope="row" className="py-4 px-6 font-medium text-gray-50 dark:text-gray-100">{address}</td>
-            <td scope="row" className="py-4 px-6 font-medium text-gray-50 dark:text-gray-100">{symbol}</td>
-            <td scope="row" className="py-4 px-6 font-medium text-gray-50 dark:text-gray-100">{totalSales}</td>
-            <td scope="row" className="py-4 px-6 font-medium text-gray-50 dark:text-gray-100">{floor}</td>
-            <td scope="row" className="py-4 px-6 font-medium text-gray-50 dark:text-gray-100">{volume}</td>
+        <tbody>
+            <tr className="items-center justify-center border-b border-gray-400">
+            <td scope="row" className="content-evenly py-2 px-6 font-medium text-gray-50 dark:text-gray-100">{name}</td>
+            <td scope="row" className="justify-center py-2 px-36 font-medium text-gray-50 dark:text-gray-100">{symbol}</td>
+            <td scope="row" className="content-center py-2 px-6 font-medium text-gray-50 dark:text-gray-100">{totalSales}</td>
+            <td scope="row" className="content-center py-2 px-6 font-medium text-gray-50 dark:text-gray-100">{floor}</td>
+            <td scope="row" className="content-center py-2 px-6 font-medium text-gray-50 dark:text-gray-100">{volume}</td>
             </tr>
-        </table>
+        </tbody>
     )
 }
--- frontend/pages/trending_nft.js
+++ frontend/pages/trending_nft.js
@@ -55,41 +55,29 @@ export default function NFT({}) {
 
     if (error)
         return (
-            <div className="mt-24 ml-24 md:ml-64 xl:ml-80 mb-16">
+            <div className="mt-20 ml-36 md:ml-64 xl:ml-88 mb-16">
                 <h1>{error}</h1>
             </div>
         )
 
     return (
-        <div className="flex mt-40 ml-20 md:ml-64 xl:ml-80 mb-22">
-        <div className="w-[42rem] grid grid-rows-1 py-2 px-4 border dark:border-gray-400 rounded-xl bg-gray-100 dark:bg-gray-850">
-            <div className="flex w-[40rem] items-center dark:border-gray-400 mb-2">
-            </div>       
-                 <table className="w-[38rem] items-center text-sm text-left table-fixed"> 
-                 {/* <div className="h-10 w-10">
-                            <img
-                                src={imageUrl}
-                                className="rounded-full"
-                                style={{
-                                    width: '40px',
-                                    height: '40px',
-                                    objectFit: 'cover',
-                                }}
-                            />
-                        </div> */}
-                 <thead className="flex items-center text-xs text-gray-100 uppercase dark:border-gray-400 dark:text-gray">
+        <div className="ml-24 px-64 py-36 ">
+        <div className="content-center w-[76rem] py-2 px-22 border dark:border-gray-400 rounded-xl bg-gray-100 dark:bg-gray-850">
+            <div className="flex dark:border-gray-400">     
+                 <table className="items-center text-sm table-fixed"> 
+                 <thead className="flex text-xs text-gray-100 uppercase dark:text-gray">
                      <tr>
-                     <th scope="col" className="py-4 px-16">Name</th>
-                     <th scope="col" className="py-4 px-14">Symbol</th>
-                     <th scope="col" className="py-4 px-1">Total Sales</th>
-                     <th scope="col" className="py-4 px-6">Floor</th>
-                     <th scope="col" className="py-4 px-6">Volume</th>
+                     <th scope="col" className="py-6 px-20">Name</th>
+                     <th scope="col" className="py-6 px-24">Symbol</th>
+                     <th scope="col" className="py-6 px-24">Total Sales</th>
+                     <th scope="col" className="py-6 px-24">Floor</th>
+                     <th scope="col" className="py-6 px-24">Volume</th>
                      </tr>
                  </thead>
                  <tbody>
                  {data &&
                     data.data.trendingCollections.edges.map((collections) => (
-                        <div className="flex w-[45rem] items-center" key={collections.node.address}>
+                        <div key={collections.node.address}>
                             <TrendingNFT
                                 name={collections.node.name}
                                 symbol={collections.node.symbol}
@@ -101,6 +89,7 @@ export default function NFT({}) {
                     ))}
                     </tbody>
             </table>
+            </div>  
             {loading && (
                 <div className="flex flex-col items-center mt-10">
                     <h1>Loading...</h1>
