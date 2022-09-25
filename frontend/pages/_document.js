863369a073d993735aafcd76b0e84b4eb80288c0
0xThresh.eth <0xthresh@protonmail.com>
2022-09-24 19:34:34 -0700

    Tried to add script, haven't been able to get it to trigger on frontend

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -1,4 +1,31 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
+
+export const networks = {
+  137: {
+    unlockAddress: "0xE8E5cd156f89F7bdB267EabD5C43Af3d5AF2A78f", // Smart contracts docs include all addresses on all networks
+    provider: "https://polygon-mainnet.g.alchemy.com/v2/53QZ031nRaLKVkyoB_jrnlXUbSqGsszC",
+  }
+}
+
+export const paywallConfig = {
+  locks: {
+    "0x1e11135Db117D67915d33d6931Bb84F6F130dD55": {
+      network: 137,
+      name: "blubbr.xyz trending nfts",
+    },
+    title: "blubbr.xyz trending nfts",
+    icon: "https://raw.githubusercontent.com/0xChampi/blubbr/main/frontend/public/logos/blubbr_logo.png",
+    metadataInputs: [
+        {
+            "name": "Name",
+            "type": "text",
+            "required": true
+        }
+    ],
+    pessimistic: true,
+  }
+}
 
 export default function Document() {
     return (
@@ -10,7 +37,7 @@ export default function Document() {
                 <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
                 <link rel="manifest" href="/site.webmanifest" />
                 <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" /> */}
-
+                <Script src="https://paywall.unlock-protocol.com/static/unlock.latest.min.js" strategy="beforeInteractive"/>
                 <link rel="preconnect" href="https://fonts.googleapis.com"></link>
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"></link>
                 <link
@@ -25,3 +52,4 @@ export default function Document() {
         </Html>
     )
 }
+
--- frontend/pages/trending_nft.js
+++ frontend/pages/trending_nft.js
@@ -119,58 +119,93 @@ export default function NFT({}) {
 
     return (
         <div className="ml-24 px-64 py-36 ">
-        <div className="content-center w-[76rem] py-2 px-22 border dark:border-gray-400 rounded-xl bg-gray-100 dark:bg-gray-850">
-            <div className="flex dark:border-gray-400">     
-                 <head>
-                  <script>
-                    (function(d, s) {
-                      var js = d.createElement(s),
-                        sc = d.getElementsByTagName(s)[0];
-                      js.src="https://paywall.unlock-protocol.com/static/unlock.latest.min.js";
-                      sc.parentNode.insertBefore(js, sc); }(document, "script"));
-                  </script>
-                  <script>
-                      var unlockProtocolConfig = {
-                          "pessimistic": true,
-                          "locks": {
-                              "0x3d5409CcE1d45233dE1D4eBDEe74b8E004abDD13": {
-                              "network": 1,
-                              "name": "blubbr.xyz trending nfts"
-                              }
-                          },
-                          "title": "blubbr.xyz trending nfts",
-                          "icon": "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.10UUFNA8oLdFdDpzt-Em_QHaHa%26pid%3DApi&f=1",
-                          "metadataInputs": [
-                              {
-                                  "name": "Name",
-                                  "type": "text",
-                                  "required": true
-                              }
-                          ]
-                      }
-                  </script>
-                 </head>
-                 <table className="items-center text-sm table-fixed"> 
-                 <thead className="flex text-xs text-gray-100 uppercase dark:text-gray">
-                     <tr>
-                     <th scope="col" className="py-6 px-20">Name</th>
-                     <th scope="col" className="py-6 px-24">Symbol</th>
-                     <th scope="col" className="py-6 px-24">Total Sales</th>
-                     <th scope="col" className="py-6 px-24">Floor</th>
-                     <th scope="col" className="py-6 px-24">Volume</th>
-                     </tr>
-                 </thead>
-                 <tbody>
-                 {data &&
-                    data.data.trendingCollections.edges.map((collections) => (
-                        <div key={collections.node.address}>
-                            <TrendingNFT
-                                name={collections.node.name}
-                                symbol={collections.node.symbol}
-                                totalSales={collections.node.stats.totalSales}
-                                floor={collections.node.stats.floor}
-                                volume={collections.node.stats.volume}
-                            />
+            {/* TimePeriod Selector */}
+            <div className="z-20 grid grid-cols-4 h-12 w-96 mb-2 border rounded-xl border-gray-350 dark:border-gray-750 bg-gray-100 dark:bg-gray-850">
+                <button
+                    onClick={() => {
+                        refetch(TRENDING_COLLECTIONS_ONE_HOUR)
+                        setTimePeriod('ONE_HOUR')
+                    }}
+                    className="h-full flex justify-center items-center"
+                >
+                    <span
+                        className={`${
+                            timePeriod == 'ONE_HOUR'
+                                ? 'py-0.5 px-4 text-gray-100 dark:text-gray-850 rounded-md bg-gradient-to-r from-purple-400 via-pink-400 to-yellow-400 dark:from-purple-300/80 dark:via-pink-300/80 dark:to-yellow-300/80 focus:ring-4 focus:outline-none focus:ring-gray-850'
+                                : ''
+                        }`}
+                    >
+                        1H
+                    </span>
+                </button>
+                <button
+                    onClick={() => {
+                        refetch(TRENDING_COLLECTIONS_TWELVE_HOURS)
+                        setTimePeriod('TWELVE_HOURS')
+                    }}
+                    className="h-full flex justify-center items-center"
+                >
+                    <span
+                        className={`${
+                            timePeriod == 'TWELVE_HOURS'
+                                ? 'py-0.5 px-4 text-gray-100 dark:text-gray-850 rounded-md bg-gradient-to-r from-purple-400 via-pink-400 to-yellow-400 dark:from-purple-300/80 dark:via-pink-300/80 dark:to-yellow-300/80 focus:ring-4 focus:outline-none focus:ring-gray-850'
+                                : ''
+                        }`}
+                    >
+                        12H
+                    </span>
+                </button>
+                <button
+                    onClick={() => {
+                        refetch(TRENDING_COLLECTIONS_ONE_DAY)
+                        setTimePeriod('ONE_DAY')
+                    }}
+                    className="h-full flex justify-center items-center"
+                >
+                    <span
+                        className={`${
+                            timePeriod == 'ONE_DAY'
+                                ? 'py-0.5 px-4 text-gray-100 dark:text-gray-850 rounded-md bg-gradient-to-r from-purple-400 via-pink-400 to-yellow-400 dark:from-purple-300/80 dark:via-pink-300/80 dark:to-yellow-300/80 focus:ring-4 focus:outline-none focus:ring-gray-850'
+                                : ''
+                        }`}
+                    >
+                        24H
+                    </span>
+                </button>
+                <button
+                    onClick={() => {
+                        refetch(TRENDING_COLLECTIONS_SEVEN_DAYS)
+                        setTimePeriod('SEVEN_DAYS')
+                    }}
+                    className="h-full flex justify-center items-center"
+                >
+                    <span
+                        className={`${
+                            timePeriod == 'SEVEN_DAYS'
+                                ? 'py-0.5 px-4 text-gray-100 dark:text-gray-850 rounded-md bg-gradient-to-r from-purple-400 via-pink-400 to-yellow-400 dark:from-purple-300/80 dark:via-pink-300/80 dark:to-yellow-300/80 focus:ring-4 focus:outline-none focus:ring-gray-850'
+                                : ''
+                        }`}
+                    >
+                        7D
+                    </span>
+                </button>
+            </div>
+
+            <div className="w-[76rem] py-3 px-6 border border-gray-350 dark:border-gray-750 rounded-xl bg-gray-100 dark:bg-gray-850">
+                {/* Upper Row */}
+                <div className="w-full h-16 pb-2 px-3 grid grid-cols-6 justify-start items-start font-bold text-lg text-black dark:text-white border-b border-gray-350 dark:border-gray-750">
+                    <div className="col-span-2 h-full flex justify-start items-end">Name</div>
+                    <div className="h-full flex justify-start items-end">Symbol</div>
+                    <div className="h-full flex justify-start items-end">Total NFT Sold</div>
+                    <div className="h-full flex justify-start items-end">Floor Price</div>
+                    <div className="h-full flex justify-start items-end">ETH Volume</div>
+                </div>
+
+                {/* Trending NFT Row */}
+                <div className="w-full px-3">
+                    {loading && (
+                        <div className="w-full h-12 flex justify-center items-center">
+                            Loading...
                         </div>
                     )}
                     {!loading &&
